d8ff3dba8cb98969fe41dd3a415199d82a2da893
HuangZiqing <270704881@qq.com>
2021-12-09 15:01:34 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='win-Auto',
-    version='1.0.1',
+    version='1.0.2',
     author='hakaboom',
     author_email='1534225986@qq.com',
     license='Apache License 2.0',
     description='',
     url='https://github.com/hakaboom/winAuto',
-    packages=['winAuto'],
+    packages=find_packages(),
     install_requires=['pywinAuto>=0.6.8',
-                      "baseImage>=1.0.6",
-                      "py-image-registration>=1.0.13",
+                      "baseImage>=1.0.8",
+                      "py-image-registration>=1.0.14",
 ],
 )
